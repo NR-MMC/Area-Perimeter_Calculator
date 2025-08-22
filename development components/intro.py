01e0f134d1ab29aeaff059f0ca008b6785dc9530
NR-MMC <naterawlinson@mmc.school.nz>
2025-08-22 21:52:23 +1200

    heading & asking whether to display instructions

---
+++ development components/intro.py
@@ -0,0 +1,38 @@
+
+# heading function: ----------------------------------------------------
+def makestatement(heading, decoration):
+    middle = heading
+    wrappingdeco = decoration * len(middle)
+    print(wrappingdeco)
+    print(middle)
+    print(wrappingdeco)
+
+# ----------------------------------------------------------------------
+
+# intro prompt function:
+def yes_no(question):
+    """checks that users enter yes / y or no / n"""
+
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            print(instructions)
+            return
+
+        elif response == "no" or response == "n":
+            return
+
+        else: #repeats question
+            print("please give a valid response \n")
+
+
+instructions = " [to be written] "
+
+
+
+#main routine: ---------------------------------------------------------
+makestatement("Nate’s Area/Perimeter Calculator", "=")
+print()
+instructprompt = yes_no("want the instructions? (Y/N)  ")
+# ----------------------------------------------------------------------
